cb3b0956d3268329be7cf47af533810f8bf453eb
Tayanne <tayh.snovaes@gmail.com>
2022-06-26 21:01:30 -0300

    add footer to login page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,5 +1,6 @@
 import { useState, FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Footer } from '../components/Footer';
 import { IgniteLogo } from '../components/IgniteLogo';
 import { useCreateSubscriberMutation } from '../graphql/generated';
 
@@ -25,57 +26,62 @@ export const Subscribe = () => {
    };
 
    return (
-      <div className='min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center'>
-         <div className='w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto'>
-            <div className='max-w-[640px]'>
-               <IgniteLogo />
+      <>
+         <div className='min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center'>
+            <div className='w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto'>
+               <div className='max-w-[640px]'>
+                  <IgniteLogo />
 
-               <h1 className='mt-8 text-[2.5rem] leading-tight'>
-                  Construa uma{' '}
-                  <strong className='text-blue-500'>aplicação completa</strong>,
-                  do zero, com <strong className='text-blue-500'>React</strong>{' '}
-                  JS
-               </h1>
-               <p className='mt-4 text-gray-200 leading-relaxed'>
-                  Em apenas uma semana você vai dominar na prática uma das
-                  tecnologias mais utilizadas e com alta demanda para acessar as
-                  melhores oportunidades do mercado.
-               </p>
-            </div>
+                  <h1 className='mt-8 text-[2.5rem] leading-tight'>
+                     Construa uma{' '}
+                     <strong className='text-blue-500'>
+                        aplicação completa
+                     </strong>
+                     , do zero, com{' '}
+                     <strong className='text-blue-500'>React</strong> JS
+                  </h1>
+                  <p className='mt-4 text-gray-200 leading-relaxed'>
+                     Em apenas uma semana você vai dominar na prática uma das
+                     tecnologias mais utilizadas e com alta demanda para acessar
+                     as melhores oportunidades do mercado.
+                  </p>
+               </div>
 
-            <div className='p-8 bg-gray-700 border border-gray-500 rounded'>
-               <strong className='text-2xl mb-6 block'>
-                  Inscreva-se gratuitamente
-               </strong>
+               <div className='p-8 bg-gray-700 border border-gray-500 rounded'>
+                  <strong className='text-2xl mb-6 block'>
+                     Inscreva-se gratuitamente
+                  </strong>
 
-               <form
-                  onSubmit={handleSubscribe}
-                  className='flex flex-col gap-2 w-full'
-               >
-                  <input
-                     type='text'
-                     placeholder='Nome completo'
-                     className='bg-gray-900 rounded px-5 h-14'
-                     onChange={(event) => setName(event.target.value)}
-                  />
-                  <input
-                     type='email'
-                     placeholder='E-mail'
-                     className='bg-gray-900 rounded px-5 h-14'
-                     onChange={(event) => setEmail(event.target.value)}
-                  />
-                  <button
-                     type='submit'
-                     disabled={loading}
-                     className='mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50'
+                  <form
+                     onSubmit={handleSubscribe}
+                     className='flex flex-col gap-2 w-full'
                   >
-                     Garantir minha vaga
-                  </button>
-               </form>
+                     <input
+                        type='text'
+                        placeholder='Nome completo'
+                        className='bg-gray-900 rounded px-5 h-14'
+                        onChange={(event) => setName(event.target.value)}
+                     />
+                     <input
+                        type='email'
+                        placeholder='E-mail'
+                        className='bg-gray-900 rounded px-5 h-14'
+                        onChange={(event) => setEmail(event.target.value)}
+                     />
+                     <button
+                        type='submit'
+                        disabled={loading}
+                        className='mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50'
+                     >
+                        Garantir minha vaga
+                     </button>
+                  </form>
+               </div>
             </div>
-         </div>
 
-         <div className='w-full max-w-[1216px] min-h-[650px] bg-codeBg bg-cover bg-no-repeat'></div>
-      </div>
+            <div className='w-full max-w-[1216px] min-h-[650px] bg-codeBg bg-cover bg-no-repeat'></div>
+         </div>
+         <Footer />
+      </>
    );
 };
